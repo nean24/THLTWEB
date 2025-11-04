055501cd59b511fb5ba5cec016fbb173911526a1
LÊ VĂN SẮC <85440972+nean24@users.noreply.github.com>
2025-11-04 20:01:32 +0700

    Refactor routes to use Route::view for clarity

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('app');
-});
+Route::view('/', 'feed.index')->name('feed');
+Route::view('/login', 'auth.login')->name('login');
+Route::view('/profile', 'profile.index')->name('profile'); còn
