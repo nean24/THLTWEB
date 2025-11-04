87cbb57c3fe02b66d57ad4f219995d16ba97a154
LÊ VĂN SẮC <85440972+nean24@users.noreply.github.com>
2025-11-04 20:01:50 +0700

    Fix profile route definition in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'feed.index')->name('feed');
 Route::view('/login', 'auth.login')->name('login');
-Route::view('/profile', 'profile.index')->name('profile'); còn
+Route::view('/profile', 'profile.index')->name('profile');
